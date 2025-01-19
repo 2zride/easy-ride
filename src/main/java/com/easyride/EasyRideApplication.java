57a7169a637a75337b388683c44e6141ed2871e1
SCY <helenason@naver.com>
2025-01-20 00:57:12 +0900

    init: 프로젝트 초기 세팅 (#1)
    * docs: 이슈 템플릿 생성
    
    * docs: PR 템플릿 생성
    
    * init: 프로젝트 구조 초기 세팅

---
+++ src/main/java/com/easyride/EasyRideApplication.java
@@ -0,0 +1,12 @@
+package com.easyride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyRideApplication.class, args);
+	}
+}
