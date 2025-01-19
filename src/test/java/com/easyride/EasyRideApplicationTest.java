8343dfebc84b820fc491857881c59a05628a8e47
SCY <helenason@naver.com>
2025-01-20 00:00:56 +0900

    init: 프로젝트 구조 초기 세팅

---
+++ src/test/java/com/easyride/EasyRideApplicationTest.java
@@ -0,0 +1,12 @@
+package com.easyride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyRideApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
