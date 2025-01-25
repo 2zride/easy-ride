79e83134fa932e14076d965ca580f723f901c79b
coli <kkwoo001021@naver.com>
2025-01-26 02:59:49 +0900

    refactor: config에 빈 애너테이션 등록

--- src/main/java/com/easyride/bus/config/BusConfig.java
+++ src/main/java/com/easyride/bus/config/BusConfig.java
@@ -1,6 +1,7 @@
 package com.easyride.bus.config;
 
 import java.time.Duration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -12,6 +13,7 @@ public class BusConfig {
     private static final Duration DEFAULT_CONNECTION_TIMEOUT = Duration.ofSeconds(60);
     private static final Duration DEFAULT_READ_TIMEOUT = Duration.ofSeconds(30);
 
+    @Bean
     public RestClient.Builder restClientBuilder() {
         return RestClient.builder()
                 .requestFactory(clientHttpRequestFactory());
